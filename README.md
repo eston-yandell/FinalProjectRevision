cccb4278c08a6c1dc3e1bd31c42aa8595ac81bbb
Eston Yandell <127790628+emperor-Wasabi@users.noreply.github.com>
2023-04-17 14:23:55 -0500

    Adding input functionality to the program.

---
+++ README.md
@@ -0,0 +1,7 @@
+Hello! This is Eston and Jacob,
+this project is for the class CSC-125. 
+Its goal is to create a simple 
+character creator for general use.
+
+It is specifically for D&D 5e, 
+and is meant as a player free fill form. 
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
+// Project by Eston Yandell and Jacob Moore
+// This is the main class for a D&D 5e Character Creator
 import java.io.*;
 
 public class Main {
-    static String[] names = {"John", "Frederick", "Flint-baby"};
 
     public static void main(String[] args) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("Character.txt"));
-            for (String name : names) {
-                writer.write("\n" + name);
-            }
+                writer.write("Welcome to the YandellMoore 5e Character Creator!");
             writer.close();
+
+            BufferedReader reader = new BufferedReader(new FileReader("Character.txt"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("Character.txt"));
-            String line;
-            while ((line = reader.readLine()) != null)
                 System.out.println(reader.readLine());
             reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader playerInput = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(playerInput.readLine()); // Takes player input
+            String str = playerInput.readLine(); // Takes string as input
+
+            BufferedReader fileRead = new BufferedReader(new FileReader("Character.txt"));
         } catch (IOException e) {
             e.printStackTrace();
         }
