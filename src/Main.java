cab1fd950b7beb9e2912f739ce53b2de1ff3ad16
Eston Yandell <127790628+emperor-Wasabi@users.noreply.github.com>
2023-04-19 15:52:19 -0500

    Changing from BufferedReader/Writer to Scanner/PrintWriter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // Project by Eston Yandell and Jacob Moore
 // This is the main class for a D&D 5e Character Creator
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -7,24 +8,35 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
+        //Initialize Vars
+        Scanner writeToFile = new Scanner(System.in);
+        writeToFile.useDelimiter("\\n");
+        File characterFile = new File("Character.txt");
 
-
-        try {
-            // Initializing Variables
-            File characterFile = new File("Character.txt");
-            Scanner writeToFile = new Scanner(System.in);
+        try (PrintWriter fw = new PrintWriter(new File(characterFile.toURI()))) {
+            // Initializing more Variables
+            String line = null;
             Scanner readFile = new Scanner(characterFile);
+            // While Loop to write and read
+            while (
+                    (line = writeToFile.nextLine()) != null
+                            &&
+                            readFile.hasNextLine()) {
+                if ((line.trim().equalsIgnoreCase("done"))) {
+                    System.out.println("Exiting");
+                    System.exit(0);
+                }
 
-            while ( readFile.hasNextLine()) {
                 String data = readFile.nextLine();
                 System.out.println(data);
-            }
+            } // While
+            fw.println(line);
 
             readFile.close();
-        }
+        } // Try
         catch (FileNotFoundException e) {
-            System.out.println("An Error occured");
+            System.out.println("An Error occurred");
             e.printStackTrace();
-        }
-    }
-}
+        } // Catch
+    } // main
+} // Main
